479aedb33852f3ae1586d1c9f04cd869a3be5efb
Asif Zaman <69311703+AsifZaman777@users.noreply.github.com>
2022-06-15 03:37:26 +0600

    updated

---
+++ user_view/homepage.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body bgcolor="#F9F1F0">
+      <h1 align="center">Star Cineplex</h1>
+    <fieldset>
+    <p><a href="#">Top Movies</a>
+     | <a href="#">Book ticket</a>
+    | <a href="#">Contact with seller</a>
+    | <a href="#">About us</a>
+    | <a href="../user_view/user_login.php">Logout</a>
+    
+ </p>
+    </fieldset>
+    <br>
+    <br>
+     <marquee behavior="" direction=""><mark> সকল দর্শকদের অবগতির জন্য জানানো যাচ্ছে যে প্রতি সপ্তাহের মঙ্গলবার সাপ্তাহিক বন্ধ থাকবে</mark></marquee>
+   <br>
+<br><br><br>
+
+    
+<div align="center">
+<img  src="../user_view/strange.jpg" alt="" width="30%">
+    <p>Movie: <a href="https://en.wikipedia.org/wiki/Doctor_Strange_in_the_Multiverse_of_Madness">Multiverse of madness</a></p>
+    <p>Director: <a href="https://en.wikipedia.org/wiki/Scott_McQuillan">Scott McQuillan</a></p>
+    <p>Cast: <a href="https://en.wikipedia.org/wiki/Scott_McQuillan">Scott McQuillan</a></p>
+    <p>Genre: <a href="https://en.wikipedia.org/wiki/Scott_McQuillan">Scott McQuillan</a></p>
+</div>
+<br><br>
+
+    <div align="center">
+   <img src="../user_view/london.jpg" width="30%" >
+   <p>Movie: <a href="https://en.wikipedia.org/wiki/London_Dreams">London Dreams</a></p>
+    <p>Director: <a href="https://en.wikipedia.org/wiki/John_Huston">John Huston</a></p>
+    <p>Cast: <a href="https://en.wikipedia.org/wiki/John_Huston">John Huston</a></p>
+    <p>Genre: <a href="https://en.wikipedia.org/wiki/John_Huston">John Huston</a></p>
+   </div>
+
+    <br><br>
+
+<div align="center">
+<img  src="../user_view/topgun.jpg" alt="" width="30%">
+<p>Movie: <a href="https://en.wikipedia.org/wiki/Top_Gun:_Maverick">Top Gun</a></p>
+<p>Director: <a href="https://en.wikipedia.org/wiki/Tony_Scott">Tony Scott</a></p>
+<p>Cast: <a href="https://en.wikipedia.org/wiki/Tony_Scott">Tony Scott</a></p>
+<p>Genre: <a href="https://en.wikipedia.org/wiki/Tony_Scott">Tony Scott</a></p>
+</div>
+
+
+
+</div>
+   
+
+</body>
+</html>
